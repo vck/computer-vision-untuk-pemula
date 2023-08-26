5347267c6c77071c76db3b038ca31f12c4f6a5ee
dasta <vickydasta@gmail.com>
2023-08-26 17:00:54 +0700

    refactor convnet script

---
+++ src/config.py
@@ -0,0 +1,6 @@
+convnet_config = {
+    "learning_rate": 0.01,
+    "ephocs": 10,
+    "num_classes": 20,
+    "num_channels": 3
+}
--- src/convnet.py
+++ src/convnet.py
@@ -1,67 +1,14 @@
+import torch
 import torch.nn as F
 import torch.nn as nn
-import torch
 from torch import flatten
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
+from torch import optim
+from torch.autograd import Variable
 
-
-class LeNet(F.Module):
-    def __init__(self, num_channels, classes):
-
-        # call parent constructor
-        super(LeNet, self).__init__()
-
-        # initialize first set of CONV => RELU => POOL layer
-
-        self.conv1 = F.Conv2d(
-            in_channels=num_channels, out_channels=20, kernel_size=(5, 5)
-        )
-
-        self.relu1 = F.ReLU()
-        self.maxpool1 = F.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-
-        # initialize second set of CONV => RELU => POOL layer
-        self.conv2 = F.Conv2d(in_channels=20, out_channels=50, kernel_size=(5, 5))
-
-        self.relu2 = F.ReLU()
-        self.maxpool2 = F.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-
-        # initialize first set of FC => RELU layers
-        self.fc1 = F.Linear(in_features=800, out_features=500)
-        self.relu3 = F.ReLU()
-
-        # initialize the softmax classifier
-        self.fc2 = F.Linear(in_features=500, out_features=classes)
-        self.logSoftmax = F.LogSoftmax(dim=1)
-
-    def forward(self, x):
-
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.maxpool1(x)
-
-        # pass the output from the prev layer thorugh the second
-
-        # set of CONV => RELU => POOL layers
-
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
-
-        # flatten the output from the prev layer and pass it through FC => RELU layers
-
-        x = flatten(x, 1)
-        x = self.fc1(x)
-        x = self.relu3(x)
-
-        # prediction
-
-        x = self.fc2(x)
-        output = self.logSoftmax(x)
-        return output
-
+# Model
 
 class CNN(nn.Module):
     def __init__(self):
@@ -93,88 +40,83 @@ class CNN(nn.Module):
         output = self.out(x)
         return output, x  # return x for visualization
 
+# Hyperparameters
 
-train_data = datasets.MNIST(
-    root="data",
-    train=True,
-    transform=ToTensor(),
-    download=True,
-)
-test_data = datasets.MNIST(root="data", train=False, transform=ToTensor())
-
-
-loaders = {
-    "train": torch.utils.data.DataLoader(
-        train_data, batch_size=100, shuffle=True, num_workers=5
-    ),
-    "test": torch.utils.data.DataLoader(
-        test_data, batch_size=100, shuffle=True, num_workers=5
-    ),
+model_config = {
+    "learning_rate": 0.01,
+    "num_ephocs": 10,
+    "num_classes": 20,
+    "num_channels": 3
 }
 
-model = LeNet(num_channels=3, classes=20)
-cnn = CNN()
+# Set Device
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Load Data
+
+train_data = datasets.MNIST(root="data",train=True,transform=ToTensor(),download=True)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=100, shuffle=True, num_workers=5)
+
+test_data = datasets.MNIST(root="data", train=False, transform=ToTensor())
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=True, num_workers=5)
+
+# Initialize network
+model = CNN().to(device)
+
+# Loss and optimizer
 loss_func = nn.CrossEntropyLoss()
-from torch import optim
+optimizer = optim.Adam(model.parameters(), lr=model_config['learning_rate'])
 
-optimizer = optim.Adam(cnn.parameters(), lr=0.01)
+model.train()
 
-print(cnn)
+# Train the model
 
-from torch.autograd import Variable
+total_step = len(train_loader)
 
-num_epochs = 10
+for epoch in range(model_config['num_ephocs']):
+    for batch_idx, (data, targets) in enumerate(train_loader):
 
+        # gives batch data, normalize x when iterate train_loader
+        
+        # Get data to cuda
+        data = data.to(device)
+        targets = targets.to(device)
 
-def train(num_epochs, cnn, loaders):
+        # Get data to correct shape
+        #data = data.reshape(data.shape[0], -1)
 
-    cnn.train()
+        # forward
 
-    # Train the model
-    total_step = len(loaders["train"])
+        scores = model(data)
+        print(scores)
+        loss = loss_func(scores[0], targets)
 
-    for epoch in range(num_epochs):
-        for i, (images, labels) in enumerate(loaders["train"]):
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
 
-            # gives batch data, normalize x when iterate train_loader
-            b_x = Variable(images)  # batch x
-            b_y = Variable(labels)  # batch y
-            output = cnn(b_x)[0]
-            loss = loss_func(output, b_y)
+        # update weight based on computed gradient
+        optimizer.step()
 
-            # clear gradients for this training step
-            optimizer.zero_grad()
-
-            # backpropagation, compute gradients
-            loss.backward()
-            # apply gradients
-            optimizer.step()
-
-            if (i + 1) % 100 == 0:
-                print(
-                    "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
-                        epoch + 1, num_epochs, i + 1, total_step, loss.item()
-                    )
-                )
-
-    torch.save(cnn, "cnn.pt")
-
-train(num_epochs, cnn, loaders)
-
-
-def test():
-    # Test the model
-    cnn.eval()
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for images, labels in loaders["test"]:
-            test_output, last_layer = cnn(images)
-            pred_y = torch.max(test_output, 1)[1].data.squeeze()
-            accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+        if (batch_idx + 1) % 100 == 0:
             print(
-                "Test Accuracy of the model on the 10000 test images: %.2f" % accuracy
+                "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
+                    epoch + 1, model_config['num_ephocs'], batch_idx + 1, total_step, loss.item()
+                )
             )
+"""
 
-
-test()
+# Model evaluation
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        test_output, last_layer = model(images)
+        pred_y = torch.max(test_output, 1)[1].data.squeeze()
+        accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+        print(
+            "Test Accuracy of the model on the 10000 test images: %.2f" % accuracy
+        )
+"""
